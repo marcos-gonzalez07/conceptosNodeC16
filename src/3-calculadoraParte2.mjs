208ab59d9a947dd35a3fc8dc159c79f17890ca56
marcos <marquitosgonzalez422@gmail.com>
2025-12-11 09:49:32 -0300

    modifique la extensión de archivos que trabajan con modulos

---
+++ src/3-calculadoraParte2.mjs
@@ -0,0 +1,7 @@
+export const multiplicar = (num1, num2) =>{
+    return num1 * num2
+}
+
+export const dividir= (num1, num2) =>{
+    return num1 / num2
+}
---
+++ src/index.mjs
@@ -0,0 +1,15 @@
+import {multiplicar, dividir} from './3-calculadoraParte2.mjs'
+/* const calculadora = require('./2-calculadoraParte1') */
+
+console.log(`hola mundo desde node.js`)
+
+/* 
+ feature/funcionalidad
+ fix/arreglo-bug
+ realease/funciones
+*/
+
+//console.log('Sumamos dos números: ', calculadora.sumar(20, 35))
+//console.log('Restamos dos números: ', calculadora.restar(20, 234))
+console.log('Multiplicamos dos números: ', multiplicar(5, 35))
+console.log('dividimos dos números: ', dividir(250, 20))
